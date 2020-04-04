585c2c4480f4d4130883cac0f95435092e1b0d78
jacen11 <jacen19931@gmail.com>
2020-04-04 21:33:36 +0300

    creature

--- app/src/main/java/ru/dpastukhov/creaturemom/model/Creature.kt
+++ app/src/main/java/ru/dpastukhov/creaturemom/model/Creature.kt
@@ -30,4 +30,9 @@
 
 package ru.dpastukhov.creaturemom.model
 
-class Creature
+data class Creature(
+    val attributes: CreatureAttributes = CreatureAttributes(),
+    val hitPoints: Int = 0,
+    val name: String = "",
+    val drawable: Int = 0
+)
